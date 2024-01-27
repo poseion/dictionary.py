0311418876df60f48e8cbe47bbdc8847c4eebc30
poseion <150646417+poseion@users.noreply.github.com>
2024-01-27 20:26:16 +0600

    Create myfamily.py

---
+++ myfamily.py
@@ -0,0 +1,11 @@
+myfamily = ("mother", "father", "sister", "brother", "sister") 
+print(myfamily) 
+
+type(myfamily)
+print(myfamily[4])
+
+myfamily.append('me') #'tuple' object has no attribute 'append'
+print(myfamily)
+
+myfamily.pop(4) #'tuple' object has no attribute 'pop'
+print(myfamily)

2a3b52720523b2262e0affa5a64e29b1d6d0593b
poseion <150646417+poseion@users.noreply.github.com>
2024-01-27 20:45:30 +0600

    Create user.py

---
+++ user.py
@@ -0,0 +1,9 @@
+dic = {}
+name=input("What is the user's name? ")
+age= input("What is the user's age? ")
+country = input("What is the user's country of birth? ")
+knowledge = input("What is the user known for? ")
+dic['name'] = name
+dic['age'] = int(age)
+dic['country'] = country
+dic['knowledge'] = knowledge
